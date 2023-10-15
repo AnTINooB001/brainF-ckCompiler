246c2df4fd89a6181460156b1e7be30a4843fd52
AnTINooB <tvershinin375@gmail.com>
2023-10-15 19:23:38 +0300

    fix

--- brainF##k compiler/brainF##k compiler.cpp
+++ brainF##k compiler/brainF##k compiler.cpp
@@ -161,16 +161,20 @@ int translate(std::string name, std::string& code, std::string input = "")
             number++;
             here.push(number);
             ofs << "\nhere" << std::to_string(number) << ":\n";
+            ofs << "//----------------------------------------------------\n";
             ofs << "\nindex++; " << "// " << i << "\n\n";
             i++;
             move_code_pointer_right(code_pointer, code.end(), i);
             break;
         }
         case ']':
+            if (here.empty())
+                throw exc("Not found [", 0);
             ofs << "}\n";                           // конец цикла
             ofs << "if(*data_pointer != 0)\n";
             ofs << "goto here" << here.top() << ";\n"; // прыгаем в начало цикла последнего необратанного цикла
             here.pop();
+            ofs << "//----------------------------------------------------\n";
             ofs << "\nindex++; " << "// " << i << "\n\n";
             i++;
             move_code_pointer_right(code_pointer, code.end(), i);
